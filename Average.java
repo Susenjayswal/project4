579fa9b9294dbc2541189979309d4f6c62cdd3eb
Susenjayswal <susenjayswal@gmail.com>
2024-07-07 11:55:19 +0530

    change the file

--- Average.java
+++ Average.java
@@ -1,37 +1,36 @@
-
+import java.util.InputMismatchException;
 import java.util.Scanner;
-class Average{
-	double avg=0;
-	int n[]=[5,6,7,8,9];
-	void get_input()
-	{
-		try {
-			for(int i=0;i<5;i++)
-			{
-				System.out.println("Enter number");
-				n[i]=s.nextInt();
-			}
-		}catch(NumberFormatException nu) {
-			System.out.println("Non-Integer Value is not acceptable");
-			
-		}
-	}
-	void display()
-	{
-		double sum=0;
-		for(int j=0;j<5;j++)
-		{
-			sum +=n[j];
-		}
-		avg=sum/5.0;
-		System.out.println("Average of given 5 numbers -- "+avg);
-	}
-}
-public class Average {
 
-	public static void main(String[] args) {
-		cal_average ca=new cal_average();
-		ca.get_input();
-		ca.display();
-	}
+class Average {
+    double avg = 0;
+    int n[] = new int[5];
+
+    void get_input() {
+        Scanner s = new Scanner(System.in);
+        try {
+            for (int i = 0; i < 5; i++) {
+                System.out.println("Enter number");
+                n[i] = s.nextInt();
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Non-Integer Value is not acceptable");
+        }
+    }
+
+    void display() {
+        double sum = 0;
+        for (int j = 0; j < 5; j++) {
+            sum += n[j];
+        }
+        avg = sum / 5.0;
+        System.out.println("Average of given 5 numbers -- " + avg);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Average ca = new Average();
+        ca.get_input();
+        ca.display();
+    }
 }
